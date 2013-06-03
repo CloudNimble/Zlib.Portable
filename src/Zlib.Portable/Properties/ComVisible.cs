55d8c5ea4e17de24a2d89d851f9a8728f33d5f35
U-CT137\bollth <bollth@CT137.ct-in.net>
2013-06-03 09:43:44 +0200

    fix: added guid and comvisible attributes

---
+++ src/Zlib.Portable/Properties/ComVisible.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ionic.Zlib.Properties
+{
+    // Zusammenfassung:
+    //     Steuert den Zugriff eines einzelnen verwalteten Typs bzw. Members oder aller
+    //     Typen in einer Assembly auf COM.
+    [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Interface | AttributeTargets.Delegate, Inherited = false)]
+    [ComVisible(true)]
+    public sealed class ComVisibleAttribute : Attribute
+    {
+        // Zusammenfassung:
+        //     Initialisiert eine neue Instanz der ComVisibleAttribute-Klasse.
+        //
+        // Parameter:
+        //   visibility:
+        //     true, um anzugeben, dass der Typ für COM sichtbar ist, andernfalls false.Der
+        //     Standardwert ist true.
+        public ComVisibleAttribute(bool visibility)
+        {
+            this.Value = visibility;
+        }
+
+        // Zusammenfassung:
+        //     Ruft einen Wert ab, der angibt, ob der COM-Typ sichtbar ist.
+        //
+        // Rückgabewerte:
+        //     true, wenn der Typ sichtbar ist, andernfalls false.Der Standardwert ist true.
+        public bool Value
+        {
+            get;
+            private set;
+        }
+    }
+}
---
+++ src/Zlib.Portable/Properties/Guid.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ionic.Zlib.Properties
+{
+    // Zusammenfassung:
+    //     Stellt eine explizite System.Guid bereit, wenn eine automatische GUID nicht
+    //     erwünscht ist.
+    [ComVisible(true)]
+    [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Interface | AttributeTargets.Delegate, Inherited = false)]
+    public sealed class GuidAttribute : Attribute
+    {
+        // Zusammenfassung:
+        //     Initializes a new instance of the System.Runtime.InteropServices.GuidAttribute
+        //     class with the specified GUID.
+        //
+        // Parameter:
+        //   guid:
+        //     Die zuzuweisende System.Guid.
+        public GuidAttribute(string guid)
+        {
+            this.Value = guid;
+        }
+
+        // Zusammenfassung:
+        //     Ruft die System.Guid der Klasse ab.
+        //
+        // Rückgabewerte:
+        //     Der System.Guid der Klasse.
+        public string Value
+        {
+            get;
+            private set;
+        }
+    }
+}
