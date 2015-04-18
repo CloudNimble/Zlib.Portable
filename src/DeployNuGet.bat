a72a84bc2ad6dcb3fcb87692e6a476b5a62a42c3
AdvancedREI <robert@advancedrei.com>
2015-04-18 10:54:01 -0700

    Encoding fixes and Strong-Name Signing
    - Fix Issue #2.
    - Fix Issue #6.

--- src/DeployNuGet.bat
+++ src/DeployNuGet.bat
@@ -5,7 +5,9 @@ set /p choice="Enter y/n: "
 del *.nupkg
 @echo on
 ".nuget/nuget.exe" pack Zlib.Portable.nuspec -symbols
+".nuget/nuget.exe" pack Zlib.Portable.Signed.nuspec -symbols
 if /i %choice% equ y (
     ".nuget/nuget.exe" push Zlib.Portable.*.nupkg
+    ".nuget/nuget.exe" push Zlib.Portable.Signed.*.nupkg
 )
 pause
--- src/Zlib.Portable/Properties/AssemblyInfo.cs
+++ src/Zlib.Portable/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Resources;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -24,5 +25,5 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.10.0.0")]
-[assembly: AssemblyFileVersion("1.10.0.0")]
+[assembly: AssemblyVersion("1.11.0.0")]
+[assembly: AssemblyFileVersion("1.11.0.0")]
